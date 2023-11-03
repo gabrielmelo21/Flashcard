158f1a461e7b91168bd41824eba01f1b45259033
Gabriel de Melo <89091392+gabrielmelo21@users.noreply.github.com>
2023-11-03 08:48:23 -0300

    init

---
+++ src/app/components/expression-generator/expression-generator.component.ts
@@ -0,0 +1,9 @@
+import {Component, ElementRef, Input, OnInit, Renderer2} from '@angular/core';
+
+@Component({
+  selector: 'app-expression-generator',
+  templateUrl: './expression-generator.component.html',
+  styleUrls: ['./expression-generator.component.css']
+})
+export class ExpressionGeneratorComponent  {
+}
