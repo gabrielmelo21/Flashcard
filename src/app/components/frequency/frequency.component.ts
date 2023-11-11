1d45b4a5af0bfd09607b59ea1299bdad4366936b
Gabriel de Melo <89091392+gabrielmelo21@users.noreply.github.com>
2023-11-11 16:24:59 -0300

    init

---
+++ src/app/components/frequency/frequency.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {map, Observable} from "rxjs";
+import {MainAPIService} from "../../services/main-api.service";
+
+@Component({
+  selector: 'app-frequency',
+  templateUrl: './frequency.component.html',
+  styleUrls: ['./frequency.component.css']
+})
+export class FrequencyComponent {
+  public listFrequency$: Observable<any> | undefined;
+
+  constructor(private mainAPI: MainAPIService) {
+    this.listFrequency()
+
+  }
+
+
+  public listFrequency(){
+    this.listFrequency$ = this.mainAPI.listFrequency();
+  }
+
+}
